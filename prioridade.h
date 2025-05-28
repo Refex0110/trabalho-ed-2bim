3558a904b8f0c5664017826dddd6653d484ad9b7
Rafael <rafapadovan07@gmail.com>
2025-05-28 00:07:02 -0300

    third commit

--- prioridade.h
+++ prioridade.h
@@ -25,6 +25,9 @@ struct tpTarefa {
 };
 
 int verificaPrioridade(char prioridade[10]);
+void inicializarTarefa(tp &dev);
+tpDev *menorCargaRemover(tpDesc desc, tpDev *devExcluir);
+void redistribuirTarefa(tpDev *dev, tpTarefa *tarefa);
 
 tpTarefa *criaTarefa(tpTicket ticket, int chegada) {
     tpTarefa *tarefa = new tpTarefa;
@@ -42,8 +45,7 @@ tpDev *criaDev(int id) {
     tpDev *dev = new tpDev;
     dev->id = id;
     dev->prox = NULL;
-    dev->tarefas.qtdTarefa = dev->tarefas.qtdTempo = 0;
-    dev->tarefas.inicio = dev->tarefas.fim = NULL;
+	inicializarTarefa(dev);
     return dev;
 }
 
@@ -52,8 +54,8 @@ void inicializarDesc(tpDesc &desc, int qtdDevs) {
     desc.inicio = desc.fim = NULL;
 }
 void inicializarTarefa(tpDev &dev){
-	descTarefa.inicio = descTarefa.fim = NULL;
-	descTarefa.qtdTarefa = descTarefa.qtdTempo =  0;
+	dev.tarefas.inicio = dev.tarefas.fim = NULL;
+	dev.qtdTarefa = dev.qtdTempo =  0;
 }
 char devsVazio(tpDesc desc) {
     return desc.inicio == NULL;
@@ -77,150 +79,152 @@ int verificaPrioridade(char prioridade[10]) {
     else
         return 1;
 }
-
-void inserirTarefa(tpTicket ticket, tpDesc &desc, int chegada) {
-    tpDev *devInicio = desc.inicio;
-    tpDev *atual = NULL;
-    tpTarefa *tarefa = criaTarefa(ticket, chegada);
-    tpTarefa *tarefaAtual = NULL;
-
+tpDev *devMenorCarga(tpDesc &desc) {
+   tpDev *temporario = desc.inicio->prox;
+    tpDev *devMenor = desc.inicio;
+	
     // Verifica até achar um desocupado
-    while (devInicio != NULL && devInicio->tarefas.qtdTarefa > 0) {
-        devInicio = devInicio->prox;
-    }
-
-    // Se encontrou um desocupado, recebe tarefa
-    if (devInicio != NULL && devInicio->tarefas.qtdTarefa == 0) {
-        devInicio->tarefas.inicio = devInicio->tarefas.fim = tarefa;
-        devInicio->tarefas.qtdTarefa++;
-        devInicio->tarefas.qtdTempo += tarefa->tempo;
-    } 
-    // Não encontrou desocupado, procura o com menor tempo
-    else {
-        devInicio = desc.inicio;
-        atual = devInicio->prox;
-        
-        // Buscar menor tempo
-        while (atual != NULL) {
-            if (devInicio->tarefas.qtdTempo > atual->tarefas.qtdTempo) {
-                devInicio = atual;
-            }
-            atual = atual->prox;
-        }
-        // Inserção de acordo com a prioridade
-        
-            // Por ser menor prioridade, entra no fim
-            if (tarefa->tipo == 1) {
-                devInicio->tarefas.fim->prox = tarefa;
-                tarefa->ant = devInicio->tarefas.fim;
-                devInicio->tarefas.fim = tarefa;
-            } else {
-                tarefaAtual = devInicio->tarefas.inicio;
-                
-                // Se a primeira tiver menor prioridade, insere no começo 
-                if (tarefa->tipo > tarefaAtual->tipo) {
-                    tarefa->prox = tarefaAtual;
-                    tarefa->prox->ant = tarefaAtual;;
-                    tarefa->ant = tarefaAtual;
-                } else {
-                    // Busca a posição correta para inserir
-                    while (tarefaAtual != NULL && tarefa->tipo <= tarefaAtual->tipo) {
-                        tarefaAtual = tarefaAtual->prox;
-                    }
-                    
-                    if (tarefaAtual == NULL) {
-                        // Insere no final
-                        devInicio->tarefas.fim->prox = tarefa;
-                        tarefa->ant = devInicio->tarefas.fim;
-                        devInicio->tarefas.fim = tarefa;
-                    } else {
-                        // Insere antes de tarefaAtual
-                        tarefa->prox = tarefaAtual;
-                        tarefa->ant = tarefaAtual->ant;
-                        if (tarefaAtual->ant != NULL) {
-                            tarefaAtual->ant->prox = tarefa;
-                        } else {
-                            devInicio->tarefas.inicio = tarefa;
-                        }
-                        tarefaAtual->ant = tarefa;
-                    }
-                }
-            }
-        }
-        
-        devInicio->tarefas.qtdTarefa++;
-        devInicio->tarefas.qtdTempo += tarefa->tempo;
-    	}
-    }
+    while(temporario != NULL){
+		if(temporario->tarefas.qtdTempo < devMenor->tarefas.qtdTempo)
+			devMenor = temporario;
+		temporario = temporario->prox;
+	}
+	return devMenor;
 }
-void removerDev(tpDesc &desc){
-	if(devsVazio(desc))
-		printf("Nao há desenvolvedores")
-	else
-	{
-		tpDev *ant = NULL;
-		tpDev *atual = desc.inicio;
-		int idRemover;
-		for(atual; atual != NULL; atual = atual->prox)
-			printf("ID: %d\nQuantidade Tarefas: %d\n",
-			atual->id,atual->tarefas.qtdTarefa);
-		printf("Informe o ID do dev que deseja remover: ");
-		scanf("%d", &idRemover);
-		clrscr();
-		if(idRemover == 0){
-			printf("\nID invalido!\n");	
-			getch();
-		}	
+void inserirTarefa(tpTicket ticket, tpDesc &desc, int chegada) {
+    tpDev *dev = devMenorCarga(desc);
+    tpTarefa *tarefa = criaTarefa(ticket, chegada);
+    tpTarefa *tarefaAtual = dev->tarefas.inicio;
+    // Inserção de acordo com a prioridade
+    if(dev->tarefas.inicio == NULL){
+    	dev->tarefas.inicio = dev->tarefas.fim = tarefa;
+	}
+	else{
+		if(dev->tarefas.inicio->tipo < tarefa->tipo){
+			dev->tarefas.inicio->ant = tarefa;
+			tarefa->prox = dev->tarefas.inicio;
+			dev->tarefas.inicio = tarefa;
+		}
 		else{
-			//retorna o ponteiro depois de printar as opções
-			atual = desc.inicio;
-			//busca o dev com o devido ID
-			while (atual != NULL && atual->id != idRemover){
-				ant = atual;
-				atual = atual->prox;
+			if(dev->tarefas.fim->tipo <= tarefa->tipo){
+				tarefa->ant = dev->tarefas.fim;
+				dev->tarefas.fim->prox = tarefa;
+				dev->tarefas.fim = tarefa;
 			}
-			if(atual == NULL)
-				printf("Dev nao encontrado");
 			else{
-				//redistribui tarefas
-				if(atual->tarefas.qtdTarefa > 0)
-					tpTarefa *tarefa = atual->tarefas.inicio;
-				while(tarefa != NULL){
-					tpTarefa *prox = tarefa->prox;
-					//encontra dev com menor carga
-					tpDev *menor = desc.inicio;
-					tpDev *temp = desc.inicio;
-					
-					while(temp != NULL){
-						if(temp != atual  && temp->tarefas.qtdTempo < menor->tarefas.qtdTempo)
-							menor = temp;
-						temp = temp->prox;
-					}
-					if(menor->tarefas.inicio == NULL){
-						menor->tarefas.inicio = menor->tarefas.fim = tarefa;
-						tarefa->prox = tarefa->ant =  NULL;
-					}
-					else{
-						tpTarefa *t = menor->tarefas.inicio;
-						tpTarefa *antT = NULL;
-						while(t != NULL && t->tipo >= tarefa->tipo){
-							antT = t;
-							t = t->prox;
-						}
-						if(antT == NULL){
-							//insere no inicio e sobrepõe a de menor prioridade	
-							tarefa->prox = menor->tarefas.inicio
-							menor->tarefas.inicio->ant = tarefa;
-							menor->tarefas.inicio = tarefa;
-							tarefa->ant = NULL;
-						}
-						
-								
-					}
-					}
-				}
+				while(tarefaAtual->tipo >= tarefa->tipo)
+					tarefaAtual = tarefaAtual->prox;
+			
+				tarefa->prox = tarefaAtual;
+				tarefa->ant = tarefaAtual->ant;
+				tarefa->ant->prox = tarefa;
+				tarefaAtual->ant= tarefa;
 			}
 		}
-		
+	}
+	dev->tarefas.qtdTarefa++;
+    dev->tarefas.qtdTempo += tarefa->tempo;
+}
+tpDev *devID(dev){
+	tpDev *dev = desc.inicio;
+	tp *atual = desc.inicio->prox;
+	//busca o dev com o devido ID
+	while (atual != NULL && dev->id != idRemover){
+		dev = atual;
+		atual = atual->prox;
+	}
+	if(dev->id == idRemover)
+		return dev;	
+}
+void redistribuirTarefa(tpDev *dev, tpTarefa *tarefa){
+	tpTarefa *tarefaAtual = dev->tarefas.inicio;
+	if(dev->tarefas.inicio == NULL){
+    	dev->tarefas.inicio = dev->tarefas.fim = tarefa;
+	}
+	else{
+		if(dev->tarefas.inicio->tipo < tarefa->tipo){
+			dev->tarefas.inicio->ant = tarefa;
+			tarefa->prox = dev->tarefas.inicio;
+			dev->tarefas.inicio = tarefa;
+		}
+		else{
+			if(dev->tarefas.fim->tipo <= tarefa->tipo){
+				tarefa->ant = dev->tarefas.fim;
+				dev->tarefas.fim->prox = tarefa;
+				dev->tarefas.fim = tarefa;
+			}
+			else{
+				while(tarefaAtual->tipo >= tarefa->tipo){
+					tarefaAtual = tarefaAtual->prox;
+				tarefa->prox = tarefaAtual;
+				tarefa->ant = tarefaAtual->ant;
+				tarefa->ant->prox = tarefa;
+				tarefaAtual->ant= tarefa;
+			}
+		}
+	}
+	dev->tarefas.qtdTarefa++;
+    dev->tarefas.qtdTempo += tarefa->tempo;
+}
+tpDev *menorCargaRemover(tpDesc desc, tpDev *devExcluir){
+	tpDev *menor = NULL;
+	tpDev *atual = desc.inicio;
+	while(atual != NULL){
+		if(atual == devExcluir)
+			atual = atual->prox;
+		else{
+			if(menor == NULL)
+				menor = atual;
+			else 
+				if(menor->tarefas.qtdTempo > atual->tarefas.qtdTempo){
+					menor = atual;	
+			atual = atual->prox;
+		}
+	}	
+	return menor;
+}
+int removerDev(tpDesc &desc, int idRemover){
+	//busca o dev com o devido ID
+	tpDev *devID = desc.inicio;
+	tpDev *anterior = NULL;
+	//busca o dev com o devido ID
+	while (devID != NULL && devID->id != idRemover){
+		anterior = devID;
+		devID = devID->prox;
+	}
+	//se o dev apontar para NULL, o id não foi encontrado
+	if(devID == NULL)
+		return 0;
+	else{
+		//redistribuir tarefas
+		// se o dev tiver tarefas, pega a tarefa atual para entregar para outro dev
+		if(devID->tarefas.qtdTarefa > 0){
+			//se tem apenas um dev, as tarefas podem ser perdidas
+			if(desc.qtdDev == 1)
+				return 2;
+			else{
+				tpTarefa *tarefa = devID->tarefas.inicio;
+				while(tarefa != NULL){
+					devID->tarefas.inicio = devID->tarefas.inicio->prox;
+					//define outro ponteiro para não perder a tarefa
+					tpTarefa *proximaTarefa = tarefa->prox;
+					//encontrar dev com menor carga
+					tpDev *menor = menorCargaRemover(desc,devID) ;
+					//se o com menor não tem tarefas, ele recebe a primeira tarefa no inicio e fim do descritor
+					redistribuirTarefa(menor, tarefa);
+					tarefa = proximaTarefa;
+				}
+				if(anterior == NULL)
+					desc.inicio = devID->prox;
+				else
+					anterior->prox = devID->prox;
+				if(devID == desc.fim){
+					desc.fim = anterior;
+					anterior->prox = NULL;
+				}
+				delete(devID);
+				desc.qtdDev
+			}
+		}
 	}
 }
